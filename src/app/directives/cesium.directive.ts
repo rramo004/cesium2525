bf0ada164fe8e3f26f6c64d16c83c8bf310c2a49
rramo004 <rramos@Richards-MacBook-Air.local>
2019-03-07 17:25:56 -0800

    Added Cesium to application

---
+++ src/app/directives/cesium.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, OnInit, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appCesium]'
+})
+export class CesiumDirective implements OnInit{
+
+  constructor(private el: ElementRef) { }
+
+  ngOnInit() {
+    const viewer = new Cesium.Viewer(this.el.nativeElement);
+  }
+
+}
