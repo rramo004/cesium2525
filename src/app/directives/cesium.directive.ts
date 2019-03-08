e092a313049a61b19c4a3c9b37230bb2636d4af9
rramo004 <rramos@Richards-MacBook-Air.local>
2019-03-08 00:15:27 -0800

    Modified to work!

--- src/app/directives/cesium.directive.ts
+++ src/app/directives/cesium.directive.ts
@@ -1,56 +1,75 @@
 import { Directive, OnInit, ElementRef } from '@angular/core';
-import * as ms from 'milsymbol';
 import { XmljsonserviceService } from '../services/xmljsonservice.service';
+import * as ms from 'milsymbol';
+import * as parse from 'xml2js';
+import { Track } from '../classes/track'
+import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
+
 
 @Directive({
   selector: '[appCesium]'
 })
 export class CesiumDirective implements OnInit{
 
+  private xml: string;
+  private tracks: {};
+  private trackClassArr: Track[];
+
   constructor(private el: ElementRef, private xmljsonService: XmljsonserviceService) { }
 
   ngOnInit() {
     const viewer = new Cesium.Viewer(this.el.nativeElement);
-      //let sym = new ms.Symbol('SUSP--------',{size:25});
-      // for (let i = 0; i < 10; i++) {
+    
+    this.xmljsonService.getXML('http://localhost:4200/assets/tracks.xml')
+    .subscribe(response => {
+       this.xml = response;
+       parse.parseString(this.xml, (err, results) => {
+        this.tracks = results;
+        
+        this.trackClassArr = [];
+        let trkTop: any[] = this.tracks['tracks'];
+        let trks: any[] = trkTop['track'];
+        for (let i = 0; i < trks.length; i++)
+        {
+          let trackClass: Track = new Track();
+          let trk: any[] = trks[i];
+          for (let index = 0; index < 1; index++) {
+            trackClass.type = trk['type'][index];
+            trackClass.lon = trk['lon'][index];
+            trackClass.lat = trk['lat'][index];
+          }
+          
+          this.trackClassArr.push(trackClass);
+        }
+        
 
-      //   viewer.entities.add(
-      //     {
-      //       position : Cesium.Cartesian3.fromDegrees(120.9605, 23.6978),
-      //       billboard : {
-      //         image : sym.asCanvas(), //Get the canvas for the billboard
-      //         //pixelOffset : new Cesium.Cartesian2(-sym.markerAnchor.x, -sym.markerAnchor.y), // Symbol offset
-      //         eyeOffset : new Cesium.Cartesian3(0.0, 0.0, 0.0), // default
-      //         horizontalOrigin : Cesium.HorizontalOrigin.LEFT, // default
-      //         verticalOrigin : Cesium.VerticalOrigin.TOP
-      //       }
-      //     });
-      // }
-      
-      // let sym2 = new ms.Symbol('sfgpewrh--mt',{
-      //   size:25,
-      //   quantity: "200",
-      //   staffComments: "For Reinforcements",
-      //   additionalInformation: "Added Support for JJ",
-      //   direction: (750*360/6400),
-      //   type: "Machine Gun",
-      //   dtg: "2912398888111",
-      //   location: "0900000.0E570306.0N"
-      // });
-      // viewer.entities.add(
-      //   {
-      //     position : Cesium.Cartesian3.fromDegrees(120.9605, 23.6978),
-      //     billboard : {
-      //       image : sym2.asCanvas(), //Get the canvas for the billboard
-      //       //pixelOffset : new Cesium.Cartesian2(-sym.markerAnchor.x, -sym.markerAnchor.y), // Symbol offset
-      //       eyeOffset : new Cesium.Cartesian3(0.0, 0.0, 0.0), // default
-      //       horizontalOrigin : Cesium.HorizontalOrigin.LEFT, // default
-      //       verticalOrigin : Cesium.VerticalOrigin.TOP
-      //     }
-      //   }
-      // );
+        for (let i = 0; i < this.trackClassArr.length; i++) {
 
-    this.xmljsonService.storeXML();
+          let sym = new ms.Symbol(
+            this.trackClassArr[i].type,
+            {size: 25,
+            quantity: "200",
+            staffComments: "For Reinforcements",
+            additionalInformation: "Added Support for JJ",
+            direction: (750*360/6400),
+            type: "Machine Gun",
+            dtg: "2912398888111",
+            location: "0900000.0E570306.0N"}
+          );
+          viewer.entities.add(
+            {
+              position : Cesium.Cartesian3.fromDegrees(this.trackClassArr[i].lon, this.trackClassArr[i].lat),
+              billboard : {
+                image : sym.asCanvas(), //Get the canvas for the billboard
+                //pixelOffset : new Cesium.Cartesian2(-sym.markerAnchor.x, -sym.markerAnchor.y), // Symbol offset
+                eyeOffset : new Cesium.Cartesian3(0.0, 0.0, 0.0), // default
+                horizontalOrigin : Cesium.HorizontalOrigin.LEFT, // default
+                verticalOrigin : Cesium.VerticalOrigin.TOP
+              }
+            }
+          )
+        }
+      });
+    });
   }
-
 }
--- src/app/services/xmljsonservice.service.ts
+++ src/app/services/xmljsonservice.service.ts
@@ -1,55 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import * as parse from 'xml2js';
-import { Track } from '../classes/track'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class XmljsonserviceService {
-  xml: string;
-  tracks: {};
-  trackClassArr: Track[];
 
-
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   public getXML(url: string) {
     return this.http.get(url, { responseType: 'text' });
   }
 
-  public convert() {
-    parse.parseString(this.xml, (err, results) => {
-      this.tracks = results;
-    });
-    this.storeTracks();
-  }
-
-  public storeXML(){
-    this.getXML('http://localhost:4200/assets/tracks.xml')
-    .subscribe(response => {
-        this.xml = response;
-        this.convert();
-    });
-  }
-
-  
-  public storeTracks() {
-    this.trackClassArr = [];
-    let trkTop: any[] = this.tracks['tracks'];
-    let trks: any[] = trkTop['track'];
-    for (let i = 0; i < trks.length; i++)
-    {
-      let trackClass: Track = new Track();
-      let trk: any[] = trks[i];
-      for (let index = 0; index < 1; index++) {
-        trackClass.type = trk['type'][index];
-        trackClass.lon = trk['lon'][index];
-        trackClass.lat = trk['lat'][index];
-      }
-      
-      this.trackClassArr.push(trackClass);
-    }
-  }
 
 }
