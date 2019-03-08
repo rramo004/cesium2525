583c58bfa21d67bdd6911b301733cb3b92752625
rramo004 <rramos@Richards-MacBook-Air.local>
2019-03-07 18:11:02 -0800

    Added xml-js and xml2js to app

--- src/app/directives/cesium.directive.ts
+++ src/app/directives/cesium.directive.ts
@@ -1,5 +1,7 @@
 import { Directive, OnInit, ElementRef } from '@angular/core';
 import * as ms from 'milsymbol';
+import * as parse from 'xml2js';
+import * as convert from 'xml-js';
 
 @Directive({
   selector: '[appCesium]'
@@ -10,13 +12,13 @@ export class CesiumDirective implements OnInit{
 
   ngOnInit() {
     const viewer = new Cesium.Viewer(this.el.nativeElement);
-    let sym = new ms.Symbol('SUSP--------',{size:25});
-    viewer.entities.add(
+      let sym = new ms.Symbol('SUSP--------',{size:25});
+      viewer.entities.add(
       {
         position : Cesium.Cartesian3.fromDegrees(-75, 40),
         billboard : {
           image : sym.asCanvas(), //Get the canvas for the billboard
-          pixelOffset : new Cesium.Cartesian2(-sym.markerAnchor.x, -sym.markerAnchor.y), // Symbol offset
+          //pixelOffset : new Cesium.Cartesian2(-sym.markerAnchor.x, -sym.markerAnchor.y), // Symbol offset
           eyeOffset : new Cesium.Cartesian3(0.0, 0.0, 0.0), // default
           horizontalOrigin : Cesium.HorizontalOrigin.LEFT, // default
           verticalOrigin : Cesium.VerticalOrigin.TOP
@@ -25,7 +27,7 @@ export class CesiumDirective implements OnInit{
 
       let sym2 = new ms.Symbol('sfgpewrh--mt',{
         size:25,
-        quantity: 200,
+        quantity: "200",
         staffComments: "For Reinforcements",
         additionalInformation: "Added Support for JJ",
         direction: (750*360/6400),
@@ -38,13 +40,30 @@ export class CesiumDirective implements OnInit{
           position : Cesium.Cartesian3.fromDegrees(-77, 28),
           billboard : {
             image : sym2.asCanvas(), //Get the canvas for the billboard
-            pixelOffset : new Cesium.Cartesian2(-sym.markerAnchor.x, -sym.markerAnchor.y), // Symbol offset
+            //pixelOffset : new Cesium.Cartesian2(-sym.markerAnchor.x, -sym.markerAnchor.y), // Symbol offset
             eyeOffset : new Cesium.Cartesian3(0.0, 0.0, 0.0), // default
             horizontalOrigin : Cesium.HorizontalOrigin.LEFT, // default
             verticalOrigin : Cesium.VerticalOrigin.TOP
           }
         }
       );
+
+      let xml = "<root>Hello xml2js!</root>";
+      parse.parseString(xml, (err, results) => {
+          console.log(results);
+      });
+      
+
+      let xml2 =
+      '<?xml version="1.0" encoding="utf-8"?>' +
+      '<note importance="high" logged="true">' +
+      '    <title>Happy</title>' +
+      '    <todo>Work</todo>' +
+      '    <todo>Play</todo>' +
+      '</note>';
+      let result1 = convert.xml2json(xml2, {compact: true, spaces: 4});
+      let result2 = convert.xml2json(xml2, {compact: false, spaces: 4});
+      console.log(result1, '\n', result2);
   }
 
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,2 +1,4 @@
 declare var Cesium;
 declare module 'milsymbol';
+declare module 'xml2js';
+declare module 'xml-js';
